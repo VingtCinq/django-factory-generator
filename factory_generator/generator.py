6a54d5087bbac4643c13474bd7b795df2cdc3bdc
Thiago Freitas <thiagosistemas3@gmail.com>
2021-03-12 16:45:06 -0300

    FACTORY_IGNORE_APPS

--- factory_generator/generator.py
+++ factory_generator/generator.py
@@ -7,16 +7,16 @@ from factory_generator import VERSION
 from factory_generator.fields_faker import FIELD_FAKER_MAP
 from factory_generator.utils import import_from_string
 
-NORMALIZE_FIELD_MAP = getattr(settings, 'FACTORY_NORMALIZE_FIELD_MAP', {})
+NORMALIZE_FIELD_MAP = getattr(settings, "FACTORY_NORMALIZE_FIELD_MAP", {})
 
 
 IGNORE_FIELDS = [
-    'ManyToOneRel',
-    'ManyToManyRel',
-    'OneToOneRel',
-    'AutoField',
-    'BigAutoField'
-] + getattr(settings, 'FACTORY_IGNORE_FIELDS', [])
+    "ManyToOneRel",
+    "ManyToManyRel",
+    "OneToOneRel",
+    "AutoField",
+    "BigAutoField",
+] + getattr(settings, "FACTORY_IGNORE_FIELDS", [])
 
 
 class FactoryFieldGenerator:
@@ -29,7 +29,7 @@ class FactoryFieldGenerator:
     def __init__(self, field, model):
         self.field = field
         self.model = model
-        self.root_dir = getattr(settings, 'FACTORY_ROOT_DIR', 'model_factories')
+        self.root_dir = getattr(settings, "FACTORY_ROOT_DIR", "model_factories")
         super(FactoryFieldGenerator, self).__init__()
 
     @property
@@ -39,15 +39,18 @@ class FactoryFieldGenerator:
         """
         if self.field_class in IGNORE_FIELDS:
             return True
-        if not self.field.editable and getattr(settings, 'FACTORY_IGNORE_NON_EDITABLE_FIELDS', True):
+        if not self.field.editable and getattr(
+            settings, "FACTORY_IGNORE_NON_EDITABLE_FIELDS", True
+        ):
             return True
         return False
 
     @property
     def field_faker_string(self):
-        assert self.field_class in FIELD_FAKER_MAP,(
-            'No FieldFaker defined for `%s`, specify a corresponding FieldFakerClass in `settings.FIELD_FAKER_MAP` or define a corresponding model in `settings.FACTORY_NORMALIZE_FIELD_MAP`' %
-            self.field_class
+        assert self.field_class in FIELD_FAKER_MAP, (
+            "No FieldFaker defined for `%s`, specify a corresponding FieldFakerClass in `settings.FIELD_FAKER_MAP` "
+            + "or define a corresponding model in `settings.FACTORY_NORMALIZE_FIELD_MAP`"
+            % self.field_class
         )
         return FIELD_FAKER_MAP[self.field_class]
 
@@ -60,7 +63,7 @@ class FactoryFieldGenerator:
         """
         Return `True` if the field has a `choices` attribute
         """
-        return hasattr(self.field, 'choices') and self.field.choices
+        return hasattr(self.field, "choices") and self.field.choices
 
     @property
     def field_class(self):
@@ -69,7 +72,7 @@ class FactoryFieldGenerator:
         match a native Django model field class
         """
         if self.has_choices:
-            return 'ChoiceField'
+            return "ChoiceField"
         class_name = self.field.__class__.__name__
         if class_name in NORMALIZE_FIELD_MAP:
             return NORMALIZE_FIELD_MAP[class_name]
@@ -80,10 +83,7 @@ class FactoryFieldGenerator:
         """
         Return the context to render the base factory class
         """
-        res = {
-            'field': self.field,
-            'model_name': self.model.__name__
-        }
+        res = {"field": self.field, "model_name": self.model.__name__}
         return res
 
     def render_choices_list(self):
@@ -91,7 +91,9 @@ class FactoryFieldGenerator:
         For a field that has a `choices` attribute
         render the list of choices
         """
-        return render_to_string('factory_generator/choiceslist.py-tpl', self.base_context)
+        return render_to_string(
+            "factory_generator/choiceslist.py-tpl", self.base_context
+        )
 
     def render(self):
         """
@@ -118,7 +120,7 @@ class PathMixin:
         Set the `root_dir` with the `FACTORY_DIR` settings or default
         to `model_factories`
         """
-        self.root_dir = getattr(settings, 'FACTORY_ROOT_DIR', 'model_factories')
+        self.root_dir = getattr(settings, "FACTORY_ROOT_DIR", "model_factories")
         super(PathMixin, self).__init__()
 
     @property
@@ -133,7 +135,7 @@ class PathMixin:
         """
         Return the root ./__init__.py file full path
         """
-        return os.path.join(self.root_directory_path, '__init__.py')
+        return os.path.join(self.root_directory_path, "__init__.py")
 
     @property
     def app_directory_path(self):
@@ -147,27 +149,27 @@ class PathMixin:
         """
         Return the ./app/__init__.py file full path
         """
-        return os.path.join(self.app_directory_path, '__init__.py')
+        return os.path.join(self.app_directory_path, "__init__.py")
 
     @property
     def app_base_directory_path(self):
         """
         Return the ./app/base/ directory full path
         """
-        return os.path.join(self.root_directory_path, self.app_label, 'base')
+        return os.path.join(self.root_directory_path, self.app_label, "base")
 
     @property
     def app_base_init_file_path(self):
         """
         Return the ./app/base/__init__.py file full path
         """
-        return os.path.join(self.app_base_directory_path, '__init__.py')
+        return os.path.join(self.app_base_directory_path, "__init__.py")
 
 
 class FactoryModelGenerator(PathMixin):
 
     """
-    Given a model `MyModel` in the app `app`, this class can generate files to 
+    Given a model `MyModel` in the app `app`, this class can generate files to
     define factory classes:
     - model_factories/app/mymodel.py defining MyModelFactoryBase
     - model_factories/app/base/mymodel.py defining MyModelFactory
@@ -181,7 +183,8 @@ class FactoryModelGenerator(PathMixin):
         self.model = model
         self.app_label = model._meta.app_label  # needed for PathMixin
         self.model_filename = "{filename}.py".format(
-            filename=self.model.__name__.lower())
+            filename=self.model.__name__.lower()
+        )
         super(FactoryModelGenerator, self).__init__()
 
     @property
@@ -213,20 +216,17 @@ class FactoryModelGenerator(PathMixin):
         module_path = "{root_dir}.{app_label}.{model_lower}".format(
             root_dir=self.root_dir,
             app_label=self.model._meta.app_label,
-            model_lower=self.model.__name__.lower()
+            model_lower=self.model.__name__.lower(),
         )
-        return {
-            'module': module_path,
-            'factory': self.factory_name
-        }
+        return {"module": module_path, "factory": self.factory_name}
 
     def get_import_string(self, import_str):
-        arr = import_str.split('.')
+        arr = import_str.split(".")
         if len(arr) == 1:
             return "import %s" % arr[0]
         else:
             last_import = arr.pop()
-            return "from %s import %s" % ('.'.join(arr), last_import)
+            return "from %s import %s" % (".".join(arr), last_import)
 
     @property
     def context_base(self):
@@ -234,36 +234,37 @@ class FactoryModelGenerator(PathMixin):
         Get the context needed for the definition of the `[Model]FactoryBase` class
         """
         res = {
-            'model_name': self.model.__name__,
-            'model_module': self.model.__module__,
-            'factory_name': self.factory_name,
-            'factory_base_name': self.factory_base_name,
-            'imports': [],
-            'version': VERSION,
-            'fields': [],
-            'choices': [],
-            'unique': [],
-            'unique_kwargs': ''
+            "model_name": self.model.__name__,
+            "model_module": self.model.__module__,
+            "factory_name": self.factory_name,
+            "factory_base_name": self.factory_base_name,
+            "imports": [],
+            "version": VERSION,
+            "fields": [],
+            "choices": [],
+            "unique": [],
+            "unique_kwargs": "",
         }
         imports = []
         for field in self.fields:
             factory = FactoryFieldGenerator(field, self.model)
             if factory.is_ignored:
                 continue
-            if hasattr(field, 'unique') and field.unique:
-                res['unique'].append(field.name)
+            if hasattr(field, "unique") and field.unique:
+                res["unique"].append(field.name)
             if factory.has_choices:
-                res['choices'].append(factory.render_choices_list())
+                res["choices"].append(factory.render_choices_list())
             if factory.field_faker_class.imports:
                 imports += factory.field_faker_class.imports
-            res['fields'].append(factory)
-            if res['unique']:
-                res['unique_kwargs'] = ', '.join(
-                    '%r' % key for key in res['unique']) + ','
+            res["fields"].append(factory)
+            if res["unique"]:
+                res["unique_kwargs"] = (
+                    ", ".join("%r" % key for key in res["unique"]) + ","
+                )
         imports = list(set(imports))
         for import_ in imports:
             import_str = self.get_import_string(import_)
-            res['imports'].append(import_str)
+            res["imports"].append(import_str)
         return res
 
     @property
@@ -274,14 +275,14 @@ class FactoryModelGenerator(PathMixin):
         factory_module_path = "{root_dir}.{app_label}.base.{model_lower}".format(
             root_dir=self.root_dir,
             app_label=self.model._meta.app_label,
-            model_lower=self.model.__name__.lower()
+            model_lower=self.model.__name__.lower(),
         )
         res = {
-            'model_name': self.model.__name__,
-            'factory_module': factory_module_path,
-            'factory_name': self.factory_name,
-            'factory_base_name': self.factory_base_name,
-            'version': VERSION
+            "model_name": self.model.__name__,
+            "factory_module": factory_module_path,
+            "factory_name": self.factory_name,
+            "factory_base_name": self.factory_base_name,
+            "version": VERSION,
         }
         return res
 
@@ -292,7 +293,7 @@ class FactoryModelGenerator(PathMixin):
         """
         if not os.path.exists(self.root_directory_path):
             os.makedirs(self.root_directory_path)
-            open(self.root_init_file_path, 'a').close()
+            open(self.root_init_file_path, "a").close()
 
     def bootstrap_app_dirs(self):
         """
@@ -305,21 +306,23 @@ class FactoryModelGenerator(PathMixin):
             os.makedirs(self.app_directory_path)
         if not os.path.exists(self.app_base_directory_path):
             os.makedirs(self.app_base_directory_path)
-            open(self.app_base_init_file_path, 'a').close()
+            open(self.app_base_init_file_path, "a").close()
 
     @property
     def render_base(self):
         """
         Get the definition of the `[Model]FactoryBase` class
         """
-        return render_to_string('factory_generator/base-factory.py-tpl', self.context_base)
+        return render_to_string(
+            "factory_generator/base-factory.py-tpl", self.context_base
+        )
 
     @property
     def render(self):
         """
         Get the definition of the `[Model]Factory` class
         """
-        return render_to_string('factory_generator/factory.py-tpl', self.context)
+        return render_to_string("factory_generator/factory.py-tpl", self.context)
 
     @property
     def app_base_factory_file_path(self):
@@ -342,7 +345,9 @@ class FactoryModelGenerator(PathMixin):
         Create the file containing the `[Model]FactoryBase` class at the following path
         - model_factories/app/base/model.py
         """
-        with open(self.app_base_factory_file_path, 'w', encoding='utf-8') as base_factory_file:
+        with open(
+            self.app_base_factory_file_path, "w", encoding="utf-8"
+        ) as base_factory_file:
             base_factory_file.write(self.render_base)
 
     def create_factory_file(self):
@@ -351,7 +356,9 @@ class FactoryModelGenerator(PathMixin):
         - model_factories/app/model.py
         """
         if not os.path.exists(self.app_factory_file_path):
-            with open(self.app_factory_file_path, 'w', encoding='utf-8') as factory_file:
+            with open(
+                self.app_factory_file_path, "w", encoding="utf-8"
+            ) as factory_file:
                 factory_file.write(self.render)
 
     def create_files(self):
@@ -381,18 +388,30 @@ class FactoryAppGenerator(PathMixin):
         """
         Render the content of `__init__.py` file which imports all factories
         """
-        return render_to_string('factory_generator/app-init.py-tpl', context)
+
+        FACTORY_IGNORE__INIT_IMPORT = getattr(
+            settings, "FACTORY_IGNORE__INIT_IMPORT", False
+        )
+
+        if not FACTORY_IGNORE__INIT_IMPORT:
+            return render_to_string("factory_generator/app-init.py-tpl", context)
+
+        return ""
 
     def create_init_file(self, imports):
         """
         Create or override the file `__init__.py` which imports all factories
         """
         if imports:
-            with open(self.app_init_file_path, 'w', encoding='utf-8') as init_file:
-                init_file.write(self.render_init_file({
-                    'imports': imports,
-                    'version': VERSION,
-                }))
+            with open(self.app_init_file_path, "w", encoding="utf-8") as init_file:
+                init_file.write(
+                    self.render_init_file(
+                        {
+                            "imports": imports,
+                            "version": VERSION,
+                        }
+                    )
+                )
 
     def create_files(self):
         """
